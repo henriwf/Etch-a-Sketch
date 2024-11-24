affaf130575dd793dd613fa258bfff0714120657
“henriwf” <henriwolfgangf@gmail.com>
2024-11-24 02:03:58 -0300

    create a button and a prompt to define grid size

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const container = document.querySelector(".container");
 
+document.querySelector('#gridSizeButton').addEventListener('click', promptMsg);
+
+function promptMsg() {
+    let gridSize = prompt("Choose a size between 16, 32 and 64");
+}
+
+
 for (let i = 0; i < 16; i++){
 const box = document.createElement("div");
 box.classList.add("box");
-container.appendChild(box);}
+
+box.addEventListener("mouseover", event =>{
+    box.style.backgroundColor = "black";
+})
+
+container.appendChild(box);
+}
