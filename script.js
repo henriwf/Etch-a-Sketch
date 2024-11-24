fb6bc700d0a94a4f46ca7f98402238126a42258f
“henriwf” <henriwolfgangf@gmail.com>
2024-11-24 01:01:41 -0300

    create container div, create 16 divs in js and append to container div

---
+++ script.js
@@ -0,0 +1,6 @@
+const container = document.querySelector(".container");
+
+for (let i = 0; i < 16; i++){
+const box = document.createElement("div");
+box.classList.add("box");
+container.appendChild(box);}
