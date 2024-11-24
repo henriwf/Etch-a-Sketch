0b5209edc6639300abf136e9606186433f05cf72
“henriwf” <henriwolfgangf@gmail.com>
2024-11-24 02:44:32 -0300

    create loop to form a square grid of evenly sized divs according to user input

--- script.js
+++ script.js
@@ -3,17 +3,43 @@ const container = document.querySelector(".container");
 document.querySelector('#gridSizeButton').addEventListener('click', promptMsg);
 
 function promptMsg() {
-    let gridSize = prompt("Choose a size between 16, 32 and 64");
+    let gridSize = prompt("Choose a size between 1 and 64");
+
+    gridSize = parseInt(gridSize);
+
+    
+    if (gridSize <= 64) {
+        createGrid(gridSize);
+    } else {
+        alert("Invalid size! Please choose a size between 1 and 64");
+    }
 }
 
+function createGrid(size) {
+    container.innerHTML = "";
 
-for (let i = 0; i < 16; i++){
-const box = document.createElement("div");
-box.classList.add("box");
 
-box.addEventListener("mouseover", event =>{
-    box.style.backgroundColor = "black";
-})
+    for (let i = 0; i < size; i++){
 
-container.appendChild(box);
+        const boxA = document.createElement("div");
+        boxA.classList.add("boxA");
+
+        container.appendChild(boxA);
+
+        for (let j = 0; j < size; j++) {
+            const box = document.createElement("div");
+            box.classList.add("box");
+
+            box.addEventListener("mouseover", event =>{
+                box.style.backgroundColor = "black";
+            })
+
+
+            boxA.appendChild(box);
+
+        }
+
+    }
+
 }
+
